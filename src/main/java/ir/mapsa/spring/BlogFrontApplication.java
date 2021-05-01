47ca7fe26f65cda5d19fbfcaa5d5c13482d10644
Your Name <you@example.com>
2021-05-01 17:00:12 +0430

    initialising spring boot

---
+++ src/main/java/ir/mapsa/spring/BlogFrontApplication.java
@@ -0,0 +1,13 @@
+package ir.mapsa.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/mapsa/spring/BlogFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mapsa.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
